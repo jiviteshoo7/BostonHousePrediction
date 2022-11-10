ac429305ca2e3fe118245a8e7c5650903c5975ca
jivitesh <jiviteshkumar.jk007@gmail.com>
2022-11-11 01:28:30 +0530

    some modification in sklearn

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import pandas as pd
 app = Flask(__name__)
 ## load the model
 regmodel = pickle.load(open('regmodel.pkl','rb'))
-scalar = pickle.load(open('scaling.pkl','rb'))
+scaler = pickle.load(open('scaling.pkl','rb'))
 
 @app.route('/')
 def home():
@@ -19,7 +19,7 @@ def predict_api():
     data=request.json['data']
     print(data)
     print(np.array(list(data.values())).reshape(1,-1))
-    new_data = scalar.transform(np.array(list(data.values())).reshape(1,-1))
+    new_data = scaler.transform(np.array(list(data.values())).reshape(1,-1))
     output = regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
@@ -27,7 +27,7 @@ def predict_api():
 @app.route('/predict',methods=['POST'])
 def predict():
     data=[float(x) for x in request.form.values()]
-    final_input=scalar.transform(np.array(data).reshape(1,-1))
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
     print(final_input)
     output=regmodel.predict(final_input)[0]
     return render_template("home.html",prediction_text="The House price prediction is {}".format(output))
